be26b512bdfc35be63ab510d101169692e201e8f
kanita04 <kam.anita.04@gmail.com>
2025-06-21 23:17:07 +0300

    Initial Commit

---
+++ MtaaCare/MtaaCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  MtaaCareApp.swift
+//  MtaaCare
+//
+//  Created by Amy Gathoni on 21/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct MtaaCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MtaaCareTests/MtaaCareTests.swift
@@ -0,0 +1,17 @@
+//
+//  MtaaCareTests.swift
+//  MtaaCareTests
+//
+//  Created by Amy Gathoni on 21/06/2025.
+//
+
+import Testing
+@testable import MtaaCare
+
+struct MtaaCareTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
